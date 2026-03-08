f29343835462c6c2aa0a28f090055490af889053
Mihailo Ilic <milic@dmi.uns.ac.rs>
2026-03-08 17:32:09 +0100

    Added persist memory function

--- frl_components/models/util.py
+++ frl_components/models/util.py
@@ -20,3 +20,25 @@ def persist_model(model, model_name, experiment_name=None):
 
     model.save(path)
     print(f"Model saved to {path}")
+
+
+
+def persist_memory(memory_df, chosen_df, name, experiment_name=None):
+    root_path = os.getcwd()
+
+    if experiment_name is not None:
+        final_path = os.path.join(root_path, "out", experiment_name)
+    else:
+        final_path = os.path.join(root_path, "out", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    
+    if not os.path.exists(final_path):
+        os.makedirs(final_path)
+
+    path = os.path.join(final_path, f"{name}.csv")
+    path_chosen = os.path.join(final_path, f'chosen_{name}.csv')
+
+    # model.save(path)
+    memory_df.to_csv(path)
+    chosen_df.to_csv(path_chosen)
+    print(f"Memory saved to {path}")
+ 
